234209a0f6c5ac83c8e0785fad0b077221e501d2
Adam Goth <adamgoth@gmail.com>
2016-06-24 17:25:23 -0600

    Initial Commit

---
+++ ios-two-player-fighter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-two-player-fighter
+//
+//  Created by Adam Goth on 6/24/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
