400900334e4cd36bc3b7c1cd80f57a94f5a3a261
Adam Goth <adamgoth@gmail.com>
2016-06-25 11:17:01 -0600

    Adds attack button disabling, game over message, restart option

--- ios-two-player-fighter/ViewController.swift
+++ ios-two-player-fighter/ViewController.swift
@@ -16,19 +16,55 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var playerTwoHp: UILabel!
     
+    @IBOutlet weak var playerOneAttackBtn: UIButton!
+    
+    @IBOutlet weak var playerOneAttackLbl: UILabel!
+    
+    @IBOutlet weak var playerTwoAttackBtn: UIButton!
+    
+    @IBOutlet weak var playerTwoAttackLbl: UILabel!
+    
+    @IBOutlet weak var restartBtn: UIButton!
+    
+    @IBOutlet weak var restartLbl: UILabel!
+    
     var player1: Player!
     var player2: Player!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        player1 = Player(name: "Player 1", startingHp: 50, attackPower: 20)
-        player2 = Player(name: "Player 2", startingHp: 50, attackPower: 20)
+        startGame()
+    }
+    
+    func startGame() {
+        player1 = Player(name: "Player 1", startingHp: 100, attackPower: 20)
+        player2 = Player(name: "Player 2", startingHp: 100, attackPower: 20)
         playerOneHp.text = "\(player1.hp) HP"
         playerTwoHp.text = "\(player2.hp) HP"
         textDisplay.text = "Press Attack to begin!"
     }
+
+    func disableButtons() {
+        playerOneAttackBtn.hidden = true
+        playerOneAttackLbl.hidden = true
+        playerTwoAttackBtn.hidden = true
+        playerTwoAttackLbl.hidden = true
+        
+        if player1.isAlive && player2.isAlive {
+            NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: #selector(ViewController.unhideAttackButtons), userInfo: nil, repeats: false)
+        } else {
+            restartBtn.hidden = false
+            restartLbl.hidden = false
+        }
+    }
     
+    func unhideAttackButtons() {
+        playerOneAttackBtn.hidden = false
+        playerOneAttackLbl.hidden = false
+        playerTwoAttackBtn.hidden = false
+        playerTwoAttackLbl.hidden = false
+    }
     
     @IBAction func onPlayerOneAttack(sender: AnyObject) {
         player2.attacked(player1.attackPower)
@@ -39,6 +75,9 @@ class ViewController: UIViewController {
             textDisplay.text = "\(player1.name) has defeated \(player2.name)"
             playerTwoHp.text = "0 HP"
         }
+        
+        disableButtons()
+        
     }
 
     @IBAction func onPlayerTwoAttack(sender: AnyObject) {
@@ -51,6 +90,20 @@ class ViewController: UIViewController {
             playerOneHp.text = "0 HP"
         }
         
+        disableButtons()
+        
     }
+    
+    @IBAction func onRestartPressed(sender: AnyObject) {
+        playerOneAttackBtn.hidden = false
+        playerOneAttackLbl.hidden = false
+        playerTwoAttackBtn.hidden = false
+        playerTwoAttackLbl.hidden = false
+        restartBtn.hidden = true
+        restartLbl.hidden = true
+        
+        startGame()
+    }
+    
 }
 
